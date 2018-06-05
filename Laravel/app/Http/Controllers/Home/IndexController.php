44d48ebfb10d2937dfcb7099d6924ea783c5d7f0
lvpeien <891110612@qq.com>
2018-06-05 14:53:20 +0800

    Laravel/

---
+++ Laravel/app/Http/Controllers/Home/IndexController.php
@@ -0,0 +1,10 @@
+<?php
+namespace App\Http\Controllers\Home;
+use App\Http\Controllers\Controller;
+class IndexController extends Controller
+{
+    public function index(){  
+        echo "Index控制器下面的index方法";  
+    }  
+}
+
